bcb58d1241d452909514c377c84358fed9e35abd
Jesus <mamporrazo@hotmail.com>
2021-09-19 21:58:03 +0200

    basic sw

---
+++ lenovo5i basic software choco.ps1
@@ -0,0 +1,23 @@
+choco install 7zip
+choco install sublimetext3
+choco install msiafterburner
+choco install brave
+choco install everything
+choco install qdir
+choco install spacesniffer
+choco install vlc
+choco install keepass
+
+#
+choco install battle.net
+choco install epicgameslauncher
+
+#
+choco install freefilesync
+choco install reflect-free
+
+#
+choco install git
+choco install sourcetree
+
+
